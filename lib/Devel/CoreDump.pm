a5f2b3cd32f18627d3de2bd3661c2324885d6991
Florian Ragwitz <rafl@debian.org>
2008-11-20 18:46:00 +0100

    Add boilerplate docs.

--- lib/Devel/CoreDump.pm
+++ lib/Devel/CoreDump.pm
@@ -10,3 +10,27 @@ our $VERSION = '0.01';
 XSLoader::load(__PACKAGE__, $VERSION);
 
 1;
+
+__END__
+
+=head1 NAME
+
+Devel::CoreDump - create core dumps of the running perl interpreter, without terminating
+
+=head1 SYNOPSIS
+
+    use Devel::CoreDump;
+
+    Devel::CoreDump->write($path);
+
+=head1 AUTHOR
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008  Florian Ragwitz
+
+This software is distributed under the terms of the BSD License
+
+=cut
